7dddd95b33262a4a34682ea5353b171be9519218
LuuPhan <luuphan@teqnological.asia>
2023-01-09 16:07:40 +0700

    Initial Commit

---
+++ movie-app-ios/movie_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_app_iosApp.swift
+//  movie-app-ios
+//
+//  Created by Luu Phan on 09/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct movie_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
